d45ebecf3246ebc284191c2e3797de3275a42899
SollyBunny <sollybunn@gmail.com>
2023-09-06 19:40:03 +0100

    padding

--- editor/save.js
+++ editor/save.js
@@ -7,6 +7,7 @@ async function saveJSON(copy) {
 }
 
 async function saveSVG(copy) {
+    const padding = 10;
     const format = `<svg id="draw" xmlns="http://www.w3.org/2000/svg" width="%w" height="%h" viewBox="0 0 10 10">
     Your browser doesn't support SVG
     <defs>
@@ -15,14 +16,15 @@ async function saveSVG(copy) {
             <feComposite in="SourceGraphic"></feComposite>
         </filter>
     </defs>
-    <g>
+    <g translate="${padding},${padding}">
         %s
     </g>
     </svg>`;
+    
     const bbox = e_draw.getBBox();
     return format
-        .replace("%w", bbox.width)
-        .replace("%h", bbox.height)
+        .replace("%w", padding * 2 + bbox.width)
+        .replace("%h", padding * 2 + bbox.height)
         .replace("%s", e_things.innerHTML)
     ;
 }
