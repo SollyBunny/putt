d40c5c4a4978dd563235807954614179ddbc7718
SollyBunny <76408142+SollyBunny@users.noreply.github.com>
2023-10-10 09:57:13 +0100

    Update wheel.js by adding comments

--- editor/wheel.js
+++ editor/wheel.js
@@ -1,13 +1,13 @@
 
 import * as things from "./things.js";
-import { mouse } from "./drag.js";
+import { mouse } from "./drag.js"; // for mouse position
 
 const e_wheel = document.getElementById("wheel");
 const e_wheelcontent = document.getElementById("wheelcontent");
 
-let wheelClickPromise = undefined;
-let wheelOpen = false;
-let wheelSelected = 2;
+let clickPromise = undefined; // the call back
+export let isOpen = false;
+let selected = 2;
 
 const data = [
 	[
@@ -42,33 +42,32 @@ const data = [
 
 { // Construct wheel
 	let wheelID = 0;
-	function wheelScroll(event) {
+	function wheelScroll(event) { // event callback for scrolling to different wheel
 		scroll(parseInt(event.target.ID));
 	}
-	function wheelClick(event) {
-		if (wheelClickPromise === undefined) return;
-		wheelClickPromise(data[wheelSelected][parseInt(event.target.ID)]);
+	function wheelClick(event) { // event callback for clicking a wheel slice
+		if (clickPromise === undefined) return; // only if the wheel is open
+		clickPromise(data[selected][parseInt(event.target.ID)]); // call the callback
 	}
-	Object.values(document.getElementsByClassName("wheel")).forEach(wheel => {
+	Object.values(document.getElementsByClassName("wheel")).forEach(wheel => { // for each wheel
 		let children = Object.values(wheel.children);
 		let title = children[0];
-		if (wheelID > 0) {
+		if (wheelID > 0) { // if not the first wheel
 			title.children[0].ID = wheelID - 1;
-			title.children[0].addEventListener("click", wheelScroll);
+			title.children[0].addEventListener("click", wheelScroll); // add a listener on the title's left arrow
 		}
-		if (wheelID < 3) {
+		if (wheelID < 3) { // if not the last wheel
 			title.children[2].ID = wheelID + 1;
-			title.children[2].addEventListener("click", wheelScroll);
+			title.children[2].addEventListener("click", wheelScroll); // add a listener on the title's right arrow
 		}
-		children.shift();
+		children.shift(); // remove the title element from the children
 		let i = 0;
-		children.forEach(el => {
-			el.ID = i;
-			el.thing = new data[wheelID][i]();
-			el.addEventListener("click", wheelClick);
-			el.innerHTML = `${el.innerText} <br> ${i + 1}`;
-			console.log(wheelID, i)
-			el.title = el.thing.desc;
+		children.forEach(el => { // for each child (slice of the wheel)
+			el.ID = i; // set ID
+			el.thing = new data[wheelID][i](); // create an instance of the object
+			el.addEventListener("click", wheelClick); // add listener
+			el.innerHTML = `${el.innerText} <br> ${i + 1}`; // add a number
+			el.title = el.thing.desc; // set description
 			i += 1;
 		})
 		wheelID += 1;
@@ -76,41 +75,39 @@ const data = [
 }
 
 export function open() {
-	wheelOpen = true;
-	const transform = camera.getTransform();
+	isOpen = true;
+	const transform = camera.getTransform(); // set position of wheel
 	e_wheel.style.left = `${mouse.x * transform.scale + transform.x}px`;
 	e_wheel.style.top = `${mouse.y * transform.scale + transform.y}px`;
-	e_wheel.style.display = "block";
-	if (wheelClickPromise !== undefined) {
-		wheelClickPromise(undefined);
-		wheelClickPromise = undefined; // just to make sure
+	e_wheel.style.display = "block"; // show wheel
+	if (clickPromise !== undefined) { // make sure there is no pending callback
+		clickPromise(undefined);
+		clickPromise = undefined; // just to make sure
 	}
-	new Promise(resolve => { wheelClickPromise = resolve; }).then(thing => {
-		wheelClickPromise = undefined;
-		close();
+	new Promise(resolve => { clickPromise = resolve; }).then(thing => { // create a promise to wait for a click
+		clickPromise = undefined;
+		close(); // close the wheel
 		if (thing === undefined) return;
-		console.log(thing);
-		
+		console.log(thing); // stub
 	});
 }
 
 export function scroll(n) {
-	wheelSelected = n;
-	console.log(n, wheelClickPromise)
-	e_wheelcontent.style.transform = `translateX(-${400 * n}px)`;
+	selected = n;
+	e_wheelcontent.style.transform = `translateX(-${400 * n}px)`; // scroll the content
 }
 
 export function close() {
-	wheelOpen = false;
+	isOpen = false;
 	e_wheel.style.display = "none";
-	if (wheelClickPromise !== undefined) {
-		wheelClickPromise(undefined);
-		wheelClickPromise = undefined; // just to make sure
+	if (clickPromise !== undefined) { // make sure there is no pending callback
+		clickPromise(undefined);
+		clickPromise = undefined; // just to make sure
 	}
 }
 
 export function toggle() {
-	if (wheelOpen === true) {
+	if (isOpen === true) {
 		close();
 	} else {
 		open();
@@ -118,7 +115,8 @@ export function toggle() {
 }
 
 export function select(n) {
-	
+	if (clickPromise === undefined) { // check if open
+	clickPromise(data[selected][n]);
 }
-
-scroll(wheelSelected);
+	
+scroll(selected); // scroll initially to surfaces
