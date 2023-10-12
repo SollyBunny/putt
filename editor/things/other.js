0a684f88e6cf77b070695f9a4a75f33e4f152a9e
SollyBunny <76408142+SollyBunny@users.noreply.github.com>
2023-10-12 07:19:59 +0000

    a few shapes in other

--- editor/things/other.js
+++ editor/things/other.js
@@ -5,18 +5,42 @@ class Other extends Thing {
 
 export class Cross extends Thing {
 	desc = "A cross or X shape"
+	elCreate() {
+		el = createElementSVG("polygon");
+		const points = "-0.2 -0.5, 0.2 -0.5, 0.2 -0.2, 0.5 -0.2, 0.5 0.2, 0.2 0.2, 0.2 0.5, -0.2 0.5, -0.2 0.2, -0.5 0.2, -0.5 -0.2, -0.2 -0.2"
+		el.setAttribute("points", points);
+		return this.el;
+	}
 }
 
 export class Tee extends Thing {
 	desc = "A T shape"
+	elCreate() {
+		el = createElementSVG("polygon");
+		const points = "-0.5 -0.5, 0.5 -0.5, 0.5 -0.1, 0.2 -0.1, 0.2 0.5, -0.2 0.5, -0.2 -0.1, -0.5 -0.1"
+		el.setAttribute("points", points);
+		return this.el;
+	}
 }
 
 export class Why extends Thing {
 	desc = "A Y shape with 3 equally spaced arms"
+	elCreate() {
+		el = createElementSVG("polygon");
+		const points = "-0.3 -0.4, 0 -0.1, 0.3 -0.4, 0.5 -0.2, 0.2 0.1, 0.2 0.5, -0.2 0.5, -0.2 0.1, -0.5 -0.2"
+		el.setAttribute("points", points);
+		return this.el;
+	}
 }
 
 export class Cone extends Thing {
 	desc = "An ice cream cone"
+	elCreate() {
+		el = createElementSVG("path");
+		const path = "M 0.5 0.5 L 0 -0.5 L -0.5 0.5 C 0 0.7 0 0.7 0.5 0.5"
+		el.setAttribute("d", path);
+		return this.el;
+	}
 }
 
 export class Cylinder extends Thing {
