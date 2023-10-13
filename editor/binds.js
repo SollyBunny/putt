8e3ab4786a27cb2880a83f9df82b3d64be837676
SollyBunny <76408142+SollyBunny@users.noreply.github.com>
2023-10-13 10:49:18 +0100

    Add unbind and comments to binds.js

--- editor/binds.js
+++ editor/binds.js
@@ -8,19 +8,40 @@ export function bind(name, conditions, fn) {
 		prev = [];
 	prev.push([conditions, fn]);
 	binds.set(name, prev);
+	return true;
+}
+
+export function unbind(name, conditions, fn) {
+	if (!binds.has(name)) return false;
+	if (conditions === undefined) // remove all binds with this name
+		return binds.delete(name)
+	let bindtemp = binds.get(name);
+	for (let i = 0; i < bindtemp.length; i++) {
+		if (fn) {
+			if (bindtemp[i][1] !== fn) continue;
+		}
+		if (thisconditions.every(condition => conditions.includes(condition)) && conditions.every(condition => thisconditions.includes(condition))) {
+			fired.splice(i, 1);
+			return true; // can't do multiple as that would modify the list as it's being iterated
+		}
+	}
+	return false;
 }
 
 export function fire(name, conditions) {
-	if (!binds.has(name))
-		return;
-	let fired = binds.get(name);
-	for (let i = 0; i < fired.length; i++) {
-		for (let thisconditions of fired[i][0]) {
+	if (!binds.has(name)) return;
+	conditions = conditions || [];               
+	let bindtemp = binds.get(name);
+	let fired = false;
+	for (let i = 0; i < bindtemp.length; i++) {
+		for (let thisconditions of bindtemp[i][0]) {
 			// check if thisconditions is a subset of conditions
 			if (thisconditions.every(condition => conditions.includes(condition))) {
-				fired[i][1]();
+				bindtemp[i][1](); // fire the callback
+				fired = true;
 			}
 		}
 	}
+	return fired;
 }
 
