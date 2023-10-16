6e6e2a07f48bd05961266f6d1008ef90c7ec29d3
SollyBunny <76408142+SollyBunny@users.noreply.github.com>
2023-10-16 09:50:34 +0000

    temp, dont look at me

--- game/engine.js
+++ game/engine.js
@@ -1,6 +1,7 @@
 
 import * as THREE from "../lib/three.js";
 import * as CANNON from "../lib/cannon.js";
+import Lazy from "../lib/lazy.js";
 
 import CannonDebugRenderer from "../lib/cannon.dbg.js"; 
 import Settings from "./settings.js";
@@ -16,9 +17,9 @@ export const camera = new THREE.PerspectiveCamera(Settings.FOV, can.width / can.
 	scene.background = new THREE.Color(0x222222);
 }
 
-{ // Fog
-	scene.fogbottom = new THREE.Object3D();
-	scene.fogbottom.material = new THREE.MeshBasicMaterial({
+scene.fogbottom = new Lazy(() => {
+	const fogbottom = new THREE.Object3D();
+	fogbottom.material = new THREE.MeshBasicMaterial({
 		color: 0xFFFFFF,
 		transparent: true,
 		opacity: 0.5,
@@ -26,15 +27,20 @@ export const camera = new THREE.PerspectiveCamera(Settings.FOV, can.width / can.
 	});
 	const geometry = new THREE.PlaneGeometry(1024, 1024);
 	for (let i = 1; i < 10; ++i) {
-		const slice = new THREE.Mesh(geometry, scene.fogbottom.material);
+		const slice = new THREE.Mesh(geometry, fogbottom.material);
 		slice.position.y = -10 - i * 2;
 		slice.rotation.x = Math.PI / -2;
-		scene.fogbottom.add(slice);
+		fogbottom.add(slice);
 	}
-	scene.add(scene.fogbottom);
-}
-
-if (Settings.DUST) { // Dust
+	return fogbottom;
+}, fogbottom => {
+	scene.add(fogbottom);
+	return fogbottom;
+}, fogbottom => {
+	scene.remove(fogbottom);
+	return fogbottom;
+});
+scene.dust = new Lazy(() => {
 	const particlesnum = 500;
 	const particlespos = new Float32Array(particlesnum * 3);
 	const particlesvel = new Float32Array(particlesnum * 3);
@@ -43,16 +49,22 @@ if (Settings.DUST) { // Dust
 		particlespos[i + 1] = Math.random() * 50 - 10;
 		particlespos[i + 2] = Math.random() * 100 - 50;
 	}
-	scene.dust = new THREE.Points(new THREE.BufferGeometry().setAttribute("position", new THREE.BufferAttribute(particlespos, 3)), new THREE.PointsMaterial({
+	const dust = new THREE.Points(new THREE.BufferGeometry().setAttribute("position", new THREE.BufferAttribute(particlespos, 3)), new THREE.PointsMaterial({
 		color: 0xFFFFFF,
 		size: 0.1,
 		sizeAttenuation: true
 	}));
-	scene.dust.num = particlesnum;
-	scene.dust.pos = particlespos;
-	scene.dust.vel = particlesvel;
-	scene.add(scene.dust);
-}
+	dust.num = particlesnum;
+	dust.pos = particlespos;
+	dust.vel = particlesvel;
+	return dust;
+}, dust => {
+	scene.add(dust);
+	return dust;
+}, dust => {
+	scene.remove(dust);
+	return dust;
+});
 
 { // World
 	world.gravity.set(0, -1, 0) // m/s²
@@ -71,9 +83,9 @@ if (Settings.DUST) { // Dust
 }
 
 export function resize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.aspect = can.clientWidth / can.clientHeight;
 	camera.updateProjectionMatrix();
-	render.setSize(window.innerWidth, window.innerHeight);
+	render.setSize(can.clientWidth, can.clientHeight);
 }
 window.addEventListener("resize", resize);
 resize();
@@ -82,43 +94,58 @@ let start = Date.now();
 let tx, to = 0, tick = 0;
 export let fps = 0;
 export let time = 0;
-export function tick(dx) {
+export function tick() {
 	// Time
 		tx = tt - to;
 		fps = 1000 / tx;
-		if (Settings.MAXFPS > 0 && fps > Settings.MAXFPS) {
-			window.requestAnimationFrame(frame);
-			return;
-		}
+		if (Settings.MAXFPS > 0 && fps > Settings.MAXFPS) return;
 		to = tt;
 		place.tick = Date.now() - start;
-	if (Settings.DUST) {
+	if (Settings.FOG) scene.fogbottom.get(); // TODO move this to a on settings
+	else scene.fogbottom.unload();
+	if (Settings.DUST) { // Dust
 		const p = new THREE.Vector3();
-		for (let i = 0; i < scene.dust.pos.length; i += 3) {
+		const dust = scene.dust.get();
+		for (let i = 0; i < dust.pos.length; i += 3) {
 			const n = Math.random();
 			if (n < 0.1) {
-				scene.dust.vel[i + 1] += n - 0.05;
+				dust.vel[i + 1] += n - 0.05;
 			} else if (n < 0.2) {
-				scene.dust.vel[i] += n - 0.1;
+				dust.vel[i] += n - 0.1;
 			} else if (n < 0.3) {
-				scene.dust.vel[i + 2] += n - 0.15;
+				dust.vel[i + 2] += n - 0.15;
 			} else if (n < 0.6) {
-				p.x = scene.dust.pos[i    ];
-				p.y = scene.dust.pos[i + 1];
-				p.z = scene.dust.pos[i + 2];
+				p.x = dust.pos[i    ];
+				p.y = dust.pos[i + 1];
+				p.z = dust.pos[i + 2];
 				if (!camera.frustum.containsPoint(p)) {
-					scene.dust.pos[i    ] = camera.position.x + Math.random() * 100 - 50;
-					scene.dust.pos[i + 1] = camera.position.y + Math.random() * 50 - 25;
-					scene.dust.pos[i + 2] = camera.position.z + Math.random() * 100 - 50;
-					scene.dust.vel[i] = scene.dust.vel[i + 1] = scene.dust.vel[i + 2] = 0;
+					dust.pos[i    ] = camera.position.x + Math.random() * 100 - 50;
+					dust.pos[i + 1] = camera.position.y + Math.random() * 50 - 25;
+					dust.pos[i + 2] = camera.position.z + Math.random() * 100 - 50;
+					dust.vel[i] = dust.vel[i + 1] = dust.vel[i + 2] = 0;
 					continue;
 				}
 			}
-			scene.dust.pos[i    ] += scene.dust.vel[i    ];
-			scene.dust.pos[i + 1] += scene.dust.vel[i + 1];
-			scene.dust.pos[i + 2] += scene.dust.vel[i + 2];
+			dust.pos[i    ] += dust.vel[i    ];
+			dust.pos[i + 1] += dust.vel[i + 1];
+			dust.pos[i + 2] += dust.vel[i + 2];
 		}
-		scene.dust.geometry.attributes.position.needsUpdate = true;
+		dust.geometry.attributes.position.needsUpdate = true;
+	} else { scene.dust.unload(); }
+	{ // Camera
+		camera.zoom = (camera.zoomtarget + camera.zoom) / 2;
+		camera.position.x = Math.sin( camera.dir[0] * Settings.SENSITIVITY) * Math.cos(camera.dir[1] * Settings.SENSITIVITY) * camera.zoom;
+		camera.position.y = Math.sin(-camera.dir[1] * Settings.SENSITIVITY) * camera.zoom;
+		camera.position.z = Math.cos( camera.dir[0] * Settings.SENSITIVITY) * Math.cos(camera.dir[1] * Settings.SENSITIVITY) * camera.zoom;
+		if (keys["c"]) {
+			camera.position.x *= 0.25;
+			camera.position.y *= 0.25;
+			camera.position.z *= 0.25;
+		}
+		camera.position.x += camera.target.x;
+		camera.position.y += camera.target.y;
+		camera.position.z += camera.target.z;
+		camera.lookAt(camera.target.x, camera.target.y, camera.target.z);
 	}
 	render.render(scene, camera);
 }
@@ -135,33 +162,6 @@ window.addEventListener("onkeyup", event => {
 	keys[k] = false;
 	if (k === "escape") {
 		window.top.postMessage(["GAME"], "*");
-	} else if (k === "m") {
-		if (debugmeshenabled === false) {
-			if (!debugmesh) {
-				debugmesh = new CannonDebugRenderer(scene, world);
-				debugmesh.material.color.set(0xFF0000);
-				debugmesh.material.wireframeLinewidth = 15;
-				debugmesh.material.linewidth = 15;
-			}
-			debugmesh.add();
-			e_debug.style.display = "contents";
-			debugmeshenabled = true;
-		} else {
-			debugmesh.remove();
-			e_debug.style.display = "none";
-			debugmeshenabled = false;
-		}
-		event.preventDefault(); // Prevent dev console stuff
-	} else if (k === "k") {
-		scene.confetti.spawn(
-			player.body.position.x,
-			player.body.position.y + 1,
-			player.body.position.z
-		);
-	} else if (k === "f") {
-		fpsenabled = !fpsenabled;
-	} else if (k === "r") {
-		place.player.die();
 	}
 });
 
--- game/things/platonic.js
+++ game/things/platonic.js
@@ -1,6 +1,8 @@
-import { Thing } from "./thing.js";
+
+import * as THREE from "../../lib/three.js";
+import * as CANNON from "../../lib/cannon.js";
+
 import { materials } from "../place.js";
-import { BoxGeometry, Mesh } from "../../lib/three.js";
 
 class Platonic extends Thing {
 	get material() { return materials.OBJ; }
@@ -14,14 +16,18 @@ export class Tetra extends Platonic {
 	}
 }
 
-const CUBEGEOMETRY = new BoxGeometry(1, 1, 1).translate(-0.5, -0.5, -0.5);
+const CUBEGEOMETRY = new THREE.BoxGeometry(1, 1, 1).translate(-0.5, -0.5, -0.5);
+const CUBESHAPE = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
 export class Cube extends Platonic {
 	get id() { return 8; }
 	get desc() { return "A cube (3D square)"; }
 	constructor(pos, mods, props) {
 		super(pos, mods, props);
-		const cube = new Mesh(CUBEGEOMETRY, this.material);
-		cube.position.set(pos[0], pos[1], pos[2]);
+		this.geoemetry = CUBEGEOMETRY.copy();
+		this.body = CUBESHAPE.copy();
+		this.body = new CANNON.Body({
+			mass: 
+		})
 	}
 }
 
@@ -40,7 +46,13 @@ export class Icose extends Platonic {
 	get desc() { return "An icosahedron (dice with 20 sides)"; }
 }
 
+const SPHEREGEOMETRY = new THREE.SphereGeometry(1, 32, 32);
 export class Sphere extends Platonic {
 	get id() { return 12; }
 	get desc() { return "A sphere or ball"; }
+	constructor(pos, mods, props) {
+		super(pos, mods, props);
+		const sphere = new Mesh(SPHEREGEOMETRY, this.material);
+		sphere.position.set(pos[0], pos[1], pos[2]);
+	}
 }
